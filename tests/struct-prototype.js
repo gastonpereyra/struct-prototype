b96e2df3ef3696e0572a79b34c673a29f2776f59
gastonpereyra <rgpxen@gmail.com>
2021-03-13 13:15:09 -0300

    Added Object and Custom function tests

--- tests/struct-prototype.js
+++ tests/struct-prototype.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 
-const struct = require('../lib/struct-prototype');
+const { structPrototype: struct } = require('../lib');
 
 describe('struct-prototype', () => {
 
@@ -50,17 +50,47 @@ describe('struct-prototype', () => {
 
 	context('When it check basic scalar types in arrays', () => {
 
-		samplesScalarTruthy.forEach(([type, sample]) => it(`Should return values if truthy ${type} is passed as structure`, () => {
+		samplesScalarTruthy.forEach(([type, sample]) => it(`Should return values if truthy ${type} is passed as structure for the every item`, () => {
+			assert.deepStrictEqual(struct({ someField: type })({ someField: sample }), { someField: sample });
+		}));
+
+		samplesScalarFalsy.forEach(([type, sample]) => it(`Should return values if falsy ${type} is passed as structure for the every item`, () => {
+			assert.deepStrictEqual(struct({ someField: type })({ someField: sample }), { someField: sample });
+		}));
+
+		// eslint-disable-next-line max-len
+		samplesScalarInvalid.forEach(([type, samples]) => samples.forEach(sample => it(`Should throw if ${type} is passed as structure for the every item with invalid sample ${typeof sample}`,
+			() => assert.throws(() => struct({ someField: type })({ someField: sample }))
+		)));
+
+		it('Should throw if data is not an array',	() => assert.throws(() => struct(['string'])('invalid')));
+	});
+
+	context('When it check basic scalar types in object', () => {
+
+		samplesScalarTruthy.forEach(([type, sample]) => it(`Should return values if truthy ${type} is passed as structure in a field`, () => {
 			assert.deepStrictEqual(struct([type])([sample]), [sample]);
 		}));
 
-		samplesScalarFalsy.forEach(([type, sample]) => it(`Should return values if falsy ${type} is passed as structure`, () => {
+		samplesScalarFalsy.forEach(([type, sample]) => it(`Should return values if falsy ${type} is passed as structure ina a field`, () => {
 			assert.deepStrictEqual(struct([type])([sample]), [sample]);
 		}));
 
 		// eslint-disable-next-line max-len
-		samplesScalarInvalid.forEach(([type, samples]) => samples.forEach(sample => it(`Should throw if ${type} is passed as structure with invalid sample ${typeof sample}`,
+		samplesScalarInvalid.forEach(([type, samples]) => samples.forEach(sample => it(`Should throw if ${type} is passed as structure in a field with invalid sample ${typeof sample}`,
 			() => assert.throws(() => struct([type])([sample]))
 		)));
+
+		it('Should throw if field in object has no structure to check',	() => assert.throws(() => struct({ someField: 'string' })({ invalid: 'invalid' })));
+		it('Should throw if data is not an object',	() => assert.throws(() => struct({ someField: 'string' })('invalid')));
+	});
+
+	context('When it check types using custom function', () => {
+
+		const isFunType = data => data === 'Fun';
+
+		it('Should return values if custom Function returns true', () => assert.strictEqual(struct(isFunType)('Fun'), 'Fun'));
+
+		it('Should throw if custom Function returns false',	() => assert.throws(() => struct(isFunType)('not fun')));
 	});
 });
